afaef499be0ea286e113c7f6c52da479f681ada6
pgonzalezfalomir <pgonzalezfalomir@gmail.com>
2025-10-23 12:19:22 +0200

    Falta actualizar + acabar Videojuego

--- src/tema2/Tema2Videojuego.java
+++ src/tema2/Tema2Videojuego.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class Tema2Videojuego {
     public static void main(String[] args) {
 
-        // Inicialización de las variables.
         int totalstats = 0;
         int totalstats2 = 0;
         int velocidad = 0;
@@ -21,13 +20,11 @@ public class Tema2Videojuego {
         int a2 = 0;
         int ronda = 1;
 
-        // Mostrar instrucciones del juego.
         Scanner sc = new Scanner(System.in);
         System.out.println("¡Bienvenido al juego! Las instrucciones son:");
         System.out.println("1. El total de las estadísticas no puede ser superior a 500");
         System.out.println("2. El valor máximo de una estadística es 200.");
 
-        // Do while para pedir las estadísticas del jugador 1.
         do {
             System.out.println("¡Turno de Player 1!");
             System.out.println("Dame la estadística de velocidad:");
@@ -37,6 +34,10 @@ public class Tema2Videojuego {
                 velocidad = 200;
                 System.out.println("Tu estadística de velocidad será 200.");
             }
+            if (velocidad <= 0) {
+                velocidad = 1;
+                System.out.println("Tu estadística de velocidad será 1.");
+            }
 
             System.out.println("Dame la estadística de ataque:");
             ataque = sc.nextInt();
@@ -44,6 +45,10 @@ public class Tema2Videojuego {
             if (ataque > 200) {
                 ataque = 200;
                 System.out.println("Tu estadística de ataque será 200.");
+            }
+            if (ataque <= 0) {
+                ataque = 1;
+                System.out.println("Tu estadística de ataque será 1.");
             }
 
             System.out.println("Dame la estadística de defensa:");
@@ -53,6 +58,10 @@ public class Tema2Videojuego {
                 defensa = 200;
                 System.out.println("Tu estadística de defensa será 200.");
             }
+            if (defensa <= 0) {
+                defensa = 1;
+                System.out.println("Tu estadística de defensa será 1.");
+            }
 
             System.out.println("Introduce la estadística de puntos de vida:");
             vida = sc.nextInt();
@@ -61,10 +70,14 @@ public class Tema2Videojuego {
                 vida = 200;
                 System.out.println("Tu estadística de puntos de vida será 200.");
             }
+            if (vida <= 0) {
+                vida = 1;
+                System.out.println("Tu estadística de puntos de vida será 1.");
+            }
             totalstats = velocidad + ataque + defensa + vida;
-        } while (totalstats > 500);
+        }
 
-        // Do while para pedir las estadísticas del jugador 2.
+        while (totalstats > 500);
         do {
             System.out.println("¡Turno de Player 2!");
             System.out.println("Dame la estadística de velocidad:");
@@ -74,6 +87,10 @@ public class Tema2Videojuego {
                 velocidad2 = 200;
                 System.out.println("Tu estadística de velocidad será 200.");
             }
+            if (velocidad2 <= 0) {
+                velocidad2 = 1;
+                System.out.println("Tu estadística de velocidad será 1.");
+            }
 
             System.out.println("Dame la estadística de ataque:");
             ataque2 = sc.nextInt();
@@ -81,6 +98,10 @@ public class Tema2Videojuego {
             if (ataque2 > 200) {
                 ataque2 = 200;
                 System.out.println("Tu estadística de ataque será 200.");
+            }
+            if (ataque2 <= 0) {
+                ataque2 = 1;
+                System.out.println("Tu estadística de ataque será 1.");
             }
 
             System.out.println("Dame la estadística de defensa:");
@@ -90,6 +111,10 @@ public class Tema2Videojuego {
                 defensa2 = 200;
                 System.out.println("Tu estadística de defensa será 200.");
             }
+            if (defensa2 <= 0) {
+                defensa2 = 1;
+                System.out.println("Tu estadística de defensa será 1.");
+            }
 
             System.out.println("Introduce la estadística de puntos de vida:");
             vida2 = sc.nextInt();
@@ -98,38 +123,83 @@ public class Tema2Videojuego {
                 vida2 = 200;
                 System.out.println("Tu estadística de puntos de vida será 200.");
             }
+            if (vida2 <= 0) {
+                vida = 1;
+                System.out.println("Tu estadística de puntos de vida será 1.");
+            }
             totalstats2 = velocidad2 + ataque2 + defensa2 + vida2;
         } while (totalstats2 > 500);
 
-
-        System.out.println("Ronda " + ronda);
-        // While vida > 0 && vida2 > 0.
+        if (velocidad == velocidad2) {
+            System.out.println("Los dos jugadores tienen la misma velocidad, se decidirá de forma aleatoria");
+            Random random = new Random();
+            int velocidad_random = 0;
+            velocidad_random = random.nextInt(3) + 1;
+            if (velocidad_random == 1) {
+                System.out.println("El más rápido será el Player 1:");
+                velocidad++;
+            } else {
+                System.out.println("El más rápido será el Player 2:");
+                velocidad2++;
+            }
+        }
         while (vida > 0 && vida2 > 0) {
-            // Comparación de velocidad.
+            System.out.println("Ronda " + ronda + ":");
             if (velocidad > velocidad2) {
-                // Player 1 ataca.
-                double reduccion = defensa2 / 400.0; // 0.0 a 0.5
+                double reduccion = defensa2 / 400.0;
                 a1 = ataque - defensa2;
                 a1 = (int) (ataque * (1 - reduccion));
-                if (a1 <= 0) a1 = 1; // daño mínimo
+                if (a1 <= 0) a1 = 1;
                 Random random = new Random();
-                vida2 = vida2 - random.nextInt(a1) + 1;
+                vida2 = vida2 - (random.nextInt(a1) + 1) / 2;
+                if (vida2 < 0) {
+                    vida2 = 0;
+                }
                 System.out.println("La vida de Player 2 ha bajado a " + vida2);
-                ronda++;
-                break;
-            } else {
-                // Player 2 ataca.
+                if (vida2 == 0) {
+                    System.out.println("Player 2 ha sido eliminado. Ganó Player 1 🥇");
+                }
+
+                reduccion = defensa / 400.0;
+                a2 = (int) (ataque2 * (1 - reduccion));
+                if (a2 <= 0) a2 = 1;
+                vida = vida - (random.nextInt(a2) + 1) / 2;
+                if (vida < 0) {
+                    vida = 0;
+                }
+                System.out.println("La vida de Player 1 ha bajado a " + vida);
+                if (vida == 0) {
+                    System.out.println("Player 1 ha sido eliminado. ¡Ganó Player 2! 🥇");
+                }
+
+            } else if (velocidad2 > velocidad) {
                 double reduccion = defensa / 400.0;
                 a2 = (int) (ataque2 * (1 - reduccion));
                 if (a2 <= 0) a2 = 1;
                 Random random = new Random();
-                vida = vida - random.nextInt(a2) + 1;
+                vida = vida - (random.nextInt(a2) + 1) / 2;
+                if (vida < 0) {
+                    vida = 0;
+                }
                 System.out.println("La vida de Player 1 ha bajado a " + vida);
+                if (vida == 0) {
+                    System.out.println("Player 1 ha sido eliminado. ¡Ganó Player 2! 🥇");
+                }
+
+                reduccion = defensa2 / 400.0;
+                a1 = ataque - defensa2;
+                a1 = (int) (ataque * (1 - reduccion));
+                if (a1 <= 0) a1 = 1;
+                vida2 = vida2 - (random.nextInt(a1) + 1) / 2;
+                if (vida2 < 0) {
+                    vida2 = 0;
+                }
+                System.out.println("La vida de Player 2 ha bajado a " + vida2);
+                if (vida2 == 0) {
+                    System.out.println("Player 2 ha sido eliminado. Ganó Player 1 🥇");
+                }
                 ronda++;
-                break;
             }
-
         }
-
     }
 }
