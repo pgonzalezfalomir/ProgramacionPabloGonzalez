953fc89b96ae679aff6858c73ce0d5f404dd26fa
Pablo G <pgonzalezfalomir@gmail.com>
2025-11-13 10:29:43 +0100

    Update Tema3Ejercicio9.java

--- src/tema3/Tema3Ejercicio9.java
+++ src/tema3/Tema3Ejercicio9.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 
 public class Tema3Ejercicio9 {
 
-    public static void triangulo (char caracter, int lineas) {
+    public static void triangulo(char caracter, int lineas) {
+        for (int i = 1; i <= lineas; i++) {
+            // Espacios a la izquierda
+            for (int j = 1; j <= lineas - i; j++) {
+                System.out.print(" ");
+            }
+            // Caracteres del triángulo (2*i - 1 para hacerlo pirámide)
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print(caracter);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
@@ -14,6 +25,7 @@ public class Tema3Ejercicio9 {
         char caracter = in.next().charAt(0);
         System.out.println("Dame el número de líneas del triángulo:");
         int lineas = in.nextInt();
+
         triangulo(caracter, lineas);
     }
 }
