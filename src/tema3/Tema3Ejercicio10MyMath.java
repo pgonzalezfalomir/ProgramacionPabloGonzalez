6548254f4dc0438cd0832e3043ec6d33caa2bb18
pgonzalezfalomir <pgonzalezfalomir@gmail.com>
2025-11-26 14:08:27 +0100

    subida ejercicios programación modular2

--- src/tema3/Tema3Ejercicio10MyMath.java
+++ src/tema3/Tema3Ejercicio10MyMath.java
@@ -7,15 +7,21 @@ import static tema3.Tema3MyMath.*;
 public class Tema3Ejercicio10MyMath {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+//TODO: procedimiento showMenu()
         System.out.println("¿Que ejercicio quieres hacer?");
         System.out.println("'1': Calcular el perímetro y el área de un círculo, cuadrado y rectángulo");
         System.out.println("'2': Detectar numeros primos o no primos");
+        System.out.println("'3': Número de dígitos de un número entero");
+        System.out.println("'4': Número de dígitos pares de un número entero");
+        System.out.println("'5': Número de dígitos impares de un número entero");
+        System.out.println("'6': Calcular factorial");
+        System.out.println("'7': Calcular factorial de forma recursiva");
+        System.out.println("'8': Número de respuestas de ecuación segundo grado");
+        System.out.println("'9': Suma de dígitos de un número entero");
         int opc = in.nextInt();
 
         switch (opc) {
             case 1:
-                // Ejercicio 1:
                 System.out.println("¿Qué figura geométrica quieres calcular?");
                 System.out.println("'c': Círculo");
                 System.out.println("'s': Cuadrado");
@@ -32,10 +38,10 @@ public class Tema3Ejercicio10MyMath {
                         int opcioncalculocirculo = in.nextInt();
 
                         if (opcioncalculocirculo == 1) {
-                            circleArea(radio);
+                            //circleArea(radio);
                             System.out.println(circleArea(radio));
                         } else if (opcioncalculocirculo == 2) {
-                            circlePerimeter(radio);
+                            //circlePerimeter(radio);
                             System.out.println(circlePerimeter(radio));
                         } else {
                             System.out.println("Valor incorrecto");
@@ -85,7 +91,6 @@ public class Tema3Ejercicio10MyMath {
                 }
                 break;
             case 2:
-                // Ejercicio 2
                 int numero = -1; // Inicializamos con un valor distinto de 0
                 System.out.println("Introduce números enteros positivos (0 para terminar):");
 
@@ -100,14 +105,52 @@ public class Tema3Ejercicio10MyMath {
                             System.out.println(numero + " no es primo.");
                         }
                     } else if (numero < 0) {
-                        System.out.println("Por favor, introduce solo números positivos.");
+                        System.out.println("Introduce solo números positivos.");
                     }
-                    // Si el número es 0, el bucle terminará automáticamente
                 }
                 System.out.println("Programa terminado.");
                 break;
             case 3:
-
+                System.out.println("Introduce un número:");
+                int num = in.nextInt();
+                System.out.println("El número tiene " + Tema3MyMath.contarDigitos(num) + " digitos");
+                break;
+            case 4:
+                System.out.println("Introduce un número:");
+                int num2 = in.nextInt();
+                System.out.println("El número tiene " + Tema3MyMath.contarDigitosPares(num2) + " dígitos pares");
+                break;
+            case 5:
+                System.out.println("Introduce un número:");
+                int num3 = in.nextInt();
+                System.out.println("El número tiene " + Tema3MyMath.contarDigitosImpares(num3) + " dígitos impares");
+                break;
+            case 6:
+                System.out.println("Introduce un número:");
+                int num4 = in.nextInt();
+                System.out.println("El factorial de " + num4 + " es " + Tema3MyMath.calcularFactorial(num4));
+                break;
+            case 7:
+                System.out.println("Introduce un número:");
+                int num5 = in.nextInt();
+                System.out.println("El factorial calculado de forma recursiva de " + num5 + " es " + Tema3MyMath.calcularFactorialRecursivo(num5));
+                break;
+            case 8:
+                System.out.println("Introduce un coeficiente");
+                int coeficiente1 = in.nextInt();
+                System.out.println("Introduce otro coeficiente");
+                int coeficiente2 = in.nextInt();
+                System.out.println("Introduce otro coeficiente");
+                int coeficiente3 = in.nextInt();
+                System.out.println("La ecuación tiene " + solucionesEcuacion(coeficiente1, coeficiente2, coeficiente3) + " soluciones");
+                break;
+            case 9:
+                System.out.println("Introduce un número:");
+                int num6 = in.nextInt();
+                System.out.println("La suma de los dígitos de " + num6 + " es " + sumaDigitos(num6));
+                break;
+            default:
+                System.out.println("Valor incorrecto");
         }
     }
     }
--- src/tema3/Tema3MyMath.java
+++ src/tema3/Tema3MyMath.java
@@ -4,39 +4,38 @@ import java.util.Scanner;
 
 public class Tema3MyMath {
 
-    public static void main(String[] args) {
-    }
+
 
     // Funciones EJ 1
-        public static double circleArea ( double radio){
-            double resultado = 3.14 * (radio * radio);
-            return resultado;
-        }
+    public static double circleArea(double radio) {
+        double resultado = 3.14 * (radio * radio);
+        return resultado;
+    }
 
-        public static double circlePerimeter ( double radio){
-            double resultado = 2 * 3.14 * radio;
-            return resultado;
-        }
+    public static double circlePerimeter(double radio) {
+        double resultado = 2 * 3.14 * radio;
+        return resultado;
+    }
 
-        public static double squareArea ( double ladocuadrado){
-            double resultado = ladocuadrado * ladocuadrado;
-            return resultado;
-        }
+    public static double squareArea(double ladocuadrado) {
+        double resultado = ladocuadrado * ladocuadrado;
+        return resultado;
+    }
 
-        public static double squarePerimeter ( double ladocuadrado){
-            double resultado = ladocuadrado * 4;
-            return resultado;
-        }
+    public static double squarePerimeter(double ladocuadrado) {
+        double resultado = ladocuadrado * 4;
+        return resultado;
+    }
 
-        public static double rectangleArea ( double ladorectangulo, double alturarectangulo){
-            double resultado = ladorectangulo * alturarectangulo;
-            return resultado;
-        }
+    public static double rectangleArea(double ladorectangulo, double alturarectangulo) {
+        double resultado = ladorectangulo * alturarectangulo;
+        return resultado;
+    }
 
-        public static double rectanglePerimeter ( double ladorectangulo, double alturarectangulo){
-            double resultado = (2 * ladorectangulo) + (2 * alturarectangulo);
-            return resultado;
-        }
+    public static double rectanglePerimeter(double ladorectangulo, double alturarectangulo) {
+        double resultado = (2 * ladorectangulo) + (2 * alturarectangulo);
+        return resultado;
+    }
 
     // Función EJ 2
 
@@ -51,4 +50,95 @@ public class Tema3MyMath {
         }
         return true;
     }
+
+    public static boolean noEsPrimo(int n) {
+        return !esPrimo(n);
     }
+
+        // Función EJ 3
+
+    public static int contarDigitos(int num) {
+        int contador = 1;
+        while (num < 1 || num > 9) {
+            num = num / 10;
+            contador++;
+        }
+        return contador;
+    }
+
+    // Funcion EJ 4
+
+    public static int contarDigitosPares(int num2) {
+        int contador = 0;
+        while (num2 > 0) {
+            int digito = num2 % 10;
+            if (digito % 2 == 0) {
+                contador++;
+            }
+            num2 = num2 / 10;
+        }
+        return contador;
+    }
+
+    // Función EJ 5
+
+    public static int contarDigitosImpares(int num3) {
+        int contador = 0;
+        while (num3 > 0) {
+            int digito = num3 % 10;
+            if (digito % 2 != 0) {
+                contador++;
+            }
+            num3 = num3 / 10;
+        }
+        return contador;
+    }
+
+    // Función EJ 6
+
+    public static int calcularFactorial(int num4) {
+        int factorial = 1;
+        for (int contador = 1; num4 >= contador; contador++) {
+            factorial = factorial * contador;
+        }
+        return factorial;
+    }
+
+    // Función EJ 7
+
+    public static int calcularFactorialRecursivo(int num5) {
+        int factorial2 = 1;
+        if (num5 == 0 || num5 == 1) {
+            return 1;
+        } else {
+            return num5 * calcularFactorialRecursivo(num5 - 1);
+        }
+    }
+
+    // Función EJ 8
+
+    public static int solucionesEcuacion (int coeficiente1, int coeficiente2, int coeficiente3) {
+        int discriminante = coeficiente2 * coeficiente2 - 4 * coeficiente1 * coeficiente3;
+        if (discriminante > 0) {
+            return 2;
+        } else if (discriminante < 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    // Función EJ 9
+
+    public static int sumaDigitos (int num6) {
+        int suma = 0;
+        while (num6 > 0) {
+            int digito = num6 % 10;
+            suma = suma + digito;
+            num6 = num6 / 10;
+        }
+        return suma;
+    }
+
+    }
+
